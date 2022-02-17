2b8e0d475b8795c2582ea8cd2aaca2b68adee17b
Brian Ramirez <brianramirez@MacBook-Pro-de-Brian.local>
2022-02-17 00:51:26 -0300

    mobile cant autoplay with sound :/

--- tipo-2/js/scripts2.js
+++ tipo-2/js/scripts2.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){if(jQuery(window).width()>480){$('.hero').parallax({imageSrc:'img/bg1.jpg',naturalWidth:600,naturalHeight:400});}
-setTimeout(function(){$('#button_chechout_hero').show();$('#button_chechout_final').show();}, 1000);});var scrollEventHandler=function()
+var scrollEventHandler=function()
 {window.scroll(0,window.pageYOffset)}
 window.addEventListener("scroll",scrollEventHandler,false);
